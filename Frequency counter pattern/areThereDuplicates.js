292f3d58bcd87dee56bac13ce77e6e2d5cbcd1be
Sedigheh Mohammadkhani <semohammadkhani@gmail.com>
2024-09-19 10:56:23 -0600

    Fix small issues

--- Frequency counter pattern/areThereDuplicates.js
+++ Frequency counter pattern/areThereDuplicates.js
@@ -2,7 +2,7 @@
 function areThereDuplicates() {
     let collection = {}
     for (let val in arguments) {
-        collection[arguments[val]] = (collectionarguments[[val]] || 0) + 1
+        collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
     }
 
     for (let key in collection) {
